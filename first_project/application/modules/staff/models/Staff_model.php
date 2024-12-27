5e05eaef939d0105d97c5712a3e84418ef2ac65e
Bansari-Siddhapura <bansari@corbitaltechnologies.com>
2024-12-27 15:12:53 +0530

    fix : error

--- first_project/application/modules/staff/models/Staff_model.php
+++ first_project/application/modules/staff/models/Staff_model.php
@@ -1,17 +1,16 @@
 <?php
 
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if (! defined('BASEPATH')) exit('No direct script access allowed');
 
-class ModelName extends CI_Model {
+class ModelName extends CI_Model
+{
 
     public $variable;
 
     public function __construct()
     {
         parent::__construct();
-        
     }
-
 }
 
 /* End of file Staff_model.php */
